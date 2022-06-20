6ba6a5d102f6f64fb9050156e24a6ca87cd277f1
sp381 <sarah.powell381@gmail.com>
2022-06-20 11:52:46 -0700

    initial comimt

---
+++ client/src/utils/reducers.js
@@ -0,0 +1,19 @@
+export const reducer = (state, action) => {
+    switch (action.type) {
+      // if action type value is the value of `UPDATE_PRODUCTS`, return a new state object with an updated products array
+      case UPDATE_PRODUCTS:
+        return {
+          ...state,
+          products: [...action.products]
+        };
+      // if action type value is the value of `UPDATE_CATEGORIES`, return a new state object with an updated categories array
+      case UPDATE_CATEGORIES:
+        return {
+          ...state,
+          categories: [...action.categories]
+        };
+  
+      default:
+        return state;
+    }
+  };
