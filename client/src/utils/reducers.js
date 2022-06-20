f2bd367cc34eada26c9907371c57d768ee575b4d
sp381 <sarah.powell381@gmail.com>
2022-06-20 12:13:09 -0700

    added new files

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,3 +1,5 @@
+import { useReducer } from 'react';
+
 export const reducer = (state, action) => {
     switch (action.type) {
       // if action type value is the value of `UPDATE_PRODUCTS`, return a new state object with an updated products array
@@ -12,8 +14,19 @@ export const reducer = (state, action) => {
           ...state,
           categories: [...action.categories]
         };
-  
+      case UPDATE_CURRENT_CATEGORY:
+        return {
+        ...state,
+        currentCategory: action.currentCategory
+        };
+
       default:
         return state;
     }
   };
+
+  export function useProductReducer(initialState) {
+    return useReducer(reducer, initialState);
+  }
+
+  
